703e053672a8a022f97ad202324b070e495080c7
Daniel Khan <89507195+Dkhan213@users.noreply.github.com>
2023-03-28 18:43:37 -0500

    Initial Commit

--- src/main/java/com/hcc/services/UserDetailServiceImpl.java
+++ src/main/java/com/hcc/services/UserDetailServiceImpl.java
@@ -2,24 +2,13 @@ package com.hcc.services;
 
 // import com.hcc.entities.User;
 // import com.hcc.repositories.UserRepository;
-import com.hcc.utils.CustomPasswordEncoder;
-import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-
 // uncomment this class once you have created all of the needed parts
 //@Service
 //public class UserDetailServiceImpl implements UserDetailsService {
 //    @Autowired
 //    CustomPasswordEncoder passwordEncoder;
 
-    @Autowired
+//    @Autowired
     // UserRepository userRepo;
 
  //   @Override
@@ -28,5 +17,5 @@ import java.util.Optional;
 //        user.setUsername(username);
 //        user.setPassword(passwordEncoder.getPasswordEncoder().encode("asdfasdf"));
       //  return userOpt.orElseThrow(() -> new UsernameNotFoundException("Invalid Credentials"));
-    }
-}
+//    }
+//}
